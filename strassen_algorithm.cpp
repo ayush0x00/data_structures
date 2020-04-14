94a851633ff85eaf29aa03d02b180bfa6133aba3
Cipher2472 <ayushtiwari5747462@gmail.com>
2020-04-15 02:57:53 +0530

    Implemented core strassens matrix multiplication algorithm

--- strassen_algorithm.cpp
+++ strassen_algorithm.cpp
@@ -2,11 +2,11 @@
 # include <vector>
 using namespace std;
 
-std::vector<std::vector<int> >matrix_a={{2,3},{5,6}};
-std::vector<std::vector<int> >matrix_b={{1,6},{9,8}};
-std::vector<std::vector<int> > result={{0,0},{0,0}};
+vector< vector<int> >matrix_a={{2,3},{5,6}};
+ vector< vector<int> >matrix_b={{1,6},{9,8}};
+ vector< vector<int> > result={{0,0},{0,0}};
 
-void strassen_mul(std::vector<std::vector<int> > a,std::vector<vector<int>> b){
+void strassen_mul( vector< vector<int> > a, vector<vector<int>> b){
   int d1=(a[0][0]+a[1][1])*(b[0][0]+b[1][1]);
   int d2=(a[1][0]+a[1][1])*b[0][0];
   int d3=(b[0][1]-b[1][1])*a[0][0];
@@ -19,7 +19,7 @@ void strassen_mul(std::vector<std::vector<int> > a,std::vector<vector<int>> b){
   int c12=d3+d5;
   int c21=d2+d4;
   int c22=d1+d3-d2+d6;
-  std::cout << c11 <<"->"<<c12<<"->"<<c21<<"->"<<c22<< '\n';
+   cout << c11 <<"->"<<c12<<"->"<<c21<<"->"<<c22<< '\n';
 }
 
 int main(){
