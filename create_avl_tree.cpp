09d06a389a74cc0ce14c979484a78d2f7336e736
Cipher2472 <ayushtiwari5747462@gmail.com>
2020-04-22 01:05:14 +0530

    Completed insertion in avl trees

--- create_avl_tree.cpp
+++ create_avl_tree.cpp
@@ -8,6 +8,15 @@ public:
   int height;
 };
 
+Node* add_node(int data){
+  Node* new_node=new Node;
+  new_node->data=data;
+  new_node->left_child=nullptr;
+  new_node->right_child=nullptr;
+  new_node->height=1;
+  return new_node;
+}
+
 int height(Node* current){
   if (current==nullptr){
     return 0;
@@ -32,6 +41,15 @@ Node* right_rotate(Node* current){  //perform rotation on tree rooted at current
   return temp_x;
 }
 
+int height_difference(Node* node){
+  if (node==nullptr){
+    return 0;
+  }
+  else{
+    return height(node->left_child)-height(node->right_child);
+  }
+}
+
 Node* left_rotate(Node* current){
   Node* temp_x= current->right_child;
   Node* temp_y=temp_x->left_child;
@@ -43,4 +61,37 @@ Node* left_rotate(Node* current){
   temp_y->height=max(height(temp_y->right_child),height(temp_y->left_child));
 
   return temp_x;
+}
+
+Node* avl_insert(Node*  node,int data){//insert node in subtree rooted at node
+  if (node==nullptr){
+    return(add_node(data))
+  }
+  else if (data< node->data){
+    node->left_child=add_node(node->left_child,data);
+  }
+  else if(data>node->data){
+    node->right_child=add_node(node->right_child,data);
+  }
+  else{
+    return node;
+  }
+}
+
+node->height=1+max(height(node->left_child),height(node->right_child));
+int diff=height_difference(node);
+
+if (diff<-1 && data>node->right_child){
+  return left_rotate(node);
+}
+if (diff>1 && data<node->left_child){
+  return right_rotate(node);
+}
+if(diff<-1 && data<node->right_child){
+node->right_child =right_rotate(node->right_child);
+return left_rotate(node)
+}
+if(diff>1 && data>node->left_child){
+  node->left_child=left_rotate(node->left_child);
+  return right_rotate(node);
 }
