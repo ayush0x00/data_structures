eac4e546ddedff1a64e961b9e9d4d9e073c65839
Cipher2472 <ayushtiwari5747462@gmail.com>
2020-04-22 13:55:55 +0530

    started debugging

--- create_avl_tree.cpp
+++ create_avl_tree.cpp
@@ -63,35 +63,66 @@ Node* left_rotate(Node* current){
   return temp_x;
 }
 
-Node* avl_insert(Node*  node,int data){//insert node in subtree rooted at node
+Node* avl_insert(Node* node,int data){//insert node in subtree rooted at node
   if (node==nullptr){
-    return(add_node(data))
+    //std::cout << node << '\n';
+    return(add_node(data));
   }
   else if (data< node->data){
-    node->left_child=add_node(node->left_child,data);
+    node->left_child=avl_insert(node->left_child,data);
   }
   else if(data>node->data){
-    node->right_child=add_node(node->right_child,data);
+    node->right_child=avl_insert(node->right_child,data);
   }
   else{
     return node;
   }
-}
+
 
 node->height=1+max(height(node->left_child),height(node->right_child));
 int diff=height_difference(node);
+std::cout << diff << '\n';
 
-if (diff<-1 && data>node->right_child){
+if (diff<-1 && data>node->right_child->data){
   return left_rotate(node);
 }
-if (diff>1 && data<node->left_child){
+if (diff>1 && data<node->left_child->data){
   return right_rotate(node);
 }
-if(diff<-1 && data<node->right_child){
+if(diff<-1 && data<node->right_child->data){
 node->right_child =right_rotate(node->right_child);
-return left_rotate(node)
+return left_rotate(node);
 }
-if(diff>1 && data>node->left_child){
+if(diff>1 && data>node->left_child->data){
   node->left_child=left_rotate(node->left_child);
   return right_rotate(node);
+}
+if(diff==1){
+  return node;
+}
+}
+
+void preorder(Node* root){
+if (root!=nullptr){
+  std::cout << root->data <<"->"<< '\n';
+  preorder(root->left_child);
+  preorder(root->right_child);
+}
+}
+
+int main(){
+  Node* root=nullptr;
+  root=avl_insert(root,67);
+  std::cout << root->data << '\n';
+  root=avl_insert(root,56);
+  std::cout << root->data << '\n';
+
+  root=avl_insert(root,34);
+  std::cout << root << '\n';
+  root=avl_insert(root,98);
+  root=avl_insert(root,58);
+  root=avl_insert(root,22);
+  root=avl_insert(root,21);
+  preorder(root);
+  return 0;
 }
