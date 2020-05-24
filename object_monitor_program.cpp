c35d0e4e4b80d5356cec5c032a9d7dda00f3bdd7
Cipher2472 <ayushtiwari5747462@gmail.com>
2020-05-25 02:14:20 +0530

    Optimized polling loop

--- object_monitor_program.cpp
+++ object_monitor_program.cpp
@@ -18,20 +18,30 @@ private:
 class waitingvehicles{
 public:
   waitingvehicles(){}
-  void printId(){
-    std::lock_guard<std::mutex> lock(_mtx);
-    for(auto &v:_vehicles){
-      std::cout << "Vehicle# " <<v.getID()<<" is now waiting in the queue"<< '\n';
-    }
+  Vehicle popback(){
+    std::unique_lock<std::mutex> ulock(_mtx);// unique lock is needed for _condn.wait()
+    _condn.wait(ulock,[this]{return !_vehicles.empty();});//pass unique lock to condition condition_variable
+/* as soon as the thread wakes up(spurious wake up by OS or after receiving a signal of notify_one/notify_all)
+the condition is checked so as to make sure that it was not a spurious wake up but rather a call was made
+using notify one*/
+
+    Vehicle v=std::move(_vehicles.back());
+    _vehicles.pop_back();
+
+    return v;
   }
 
+
   void pushback(Vehicle &&v){
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
     std::lock_guard<std::mutex> ulock(_mtx);
     std::cout << "Vehicle# " <<v.getID()<<" will be added to the queue " <<'\n';
-    _vehicles.emplace_back(std::move(v));
+    _vehicles.push_back(std::move(v));
+    _condn.notify_one(); //notify client(popback function) after pushing new vehicle into vector
     std::this_thread::sleep_for(std::chrono::milliseconds(500));//for checking data races
   }
 private:
+  std::condition_variable _condn;
   std::mutex _mtx;
   std::vector<Vehicle> _vehicles;
 };
@@ -48,6 +58,10 @@ int main(){
   });
 
   std::cout << "Collecting results...." << '\n';
-  queue->printId();
+  while(true){
+    //popback is waiting for a new element to be available in queue
+    Vehicle v=queue->popback();
+    std::cout << "Vehicle# " <<v.getID()<<" has been removed from queue "<< '\n';
+  }
   return 0;
 }
