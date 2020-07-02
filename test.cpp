4a0ada592d6d457590840b56cb5d1e5baafff25a
Cipher2472 <ayushtiwari5747462@gmail.com>
2020-07-02 23:21:20 +0530

    commit

---
+++ test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cstdio>
+
+void use_printf(){  //use printf and scanf for faster input-output
+  printf("%s\n","Hello world" );
+  std::cout << "Hello world from cout" << '\n';
+  return 0;
+}
